07a5495a98e4252e35d9d841d3244cab4a36973a
Ryan Beltran <rp.beltran@yahoo.com>
2024-09-28 10:57:04 -0700

    temp story photos

---
+++ assets/js/story.js
@@ -0,0 +1,137 @@
+
+jQuery(document).ready(function () {
+
+   jQuery("#gallery_1").nanogallery2( {
+       thumbnailHeight:  300,
+       thumbnailWidth:   'auto',
+       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+
+       thumbnailHoverEffect2:      'scale120',
+       // ### gallery content ### 
+       items: [
+           
+           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+         ]
+     });
+
+   jQuery("#gallery_2").nanogallery2( {
+       // ### gallery settings ### 
+       thumbnailHeight:  150,
+       thumbnailWidth:   150,
+       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       
+       // ### gallery content ### 
+       items: [
+           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+         ]
+     });
+
+   jQuery("#gallery_3").nanogallery2( {
+       // ### gallery settings ### 
+       thumbnailHeight:  243 * 3,
+       thumbnailWidth:   324 * 3,
+       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+       itemsBaseURL:     './assets/images/story/',
+       
+       // ### gallery content ### 
+       items: [
+            { src: 'mt_tam.jpeg', srct: 'mt_tam_thumb.jpeg', title: 'First known photo of the couple' },
+         ]
+     });
+
+   jQuery("#gallery_4").nanogallery2( {
+       // ### gallery settings ### 
+       thumbnailHeight:  150,
+       thumbnailWidth:   150,
+       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       
+       // ### gallery content ### 
+       items: [
+           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+         ]
+     });
+
+   jQuery("#gallery_5").nanogallery2( {
+       // ### gallery settings ### 
+       thumbnailHeight:  150,
+       thumbnailWidth:   150,
+       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       
+       // ### gallery content ### 
+       items: [
+           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+         ]
+     });
+
+   jQuery("#gallery_6").nanogallery2( {
+       // ### gallery settings ### 
+       thumbnailHeight:  150,
+       thumbnailWidth:   150,
+       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       
+       // ### gallery content ### 
+       items: [
+           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+         ]
+     });
+ 
+    jQuery("#gallery_7").nanogallery2( {
+        // ### gallery settings ### 
+        thumbnailHeight:  150,
+        thumbnailWidth:   150,
+        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+        itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+        
+        // ### gallery content ### 
+        items: [
+            { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+            { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+            { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+          ]
+      });
+ 
+    jQuery("#gallery_8").nanogallery2( {
+        // ### gallery settings ### 
+        thumbnailHeight:  150,
+        thumbnailWidth:   150,
+        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+        itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+        
+        // ### gallery content ### 
+        items: [
+            { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+            { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+            { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+          ]
+      });
+
+    jQuery("#gallery_9").nanogallery2( {
+        // ### gallery settings ### 
+        thumbnailHeight:  150,
+        thumbnailWidth:   150,
+        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+        itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+        
+        // ### gallery content ### 
+        items: [
+            { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
+            { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
+            { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+          ]
+      });
+
+ });
