60aaf25ca82ee2714ee073a419a2db7a3aa8996d
Ryan Beltran <rp.beltran@yahoo.com>
2024-09-28 17:47:49 -0700

    select photos

--- assets/js/story.js
+++ assets/js/story.js
@@ -5,31 +5,34 @@ jQuery(document).ready(function () {
        thumbnailHeight:  300,
        thumbnailWidth:   'auto',
        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
-
+       itemsBaseURL:     './assets/images/story/',
+       thumbnailLabel: { display: false },
        thumbnailHoverEffect2:      'scale120',
        // ### gallery content ### 
        items: [
-           
-           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+           { src: 'marmot_sit_2.jpeg', srct: 'marmot_sit_2.jpeg', title: 'Kings Canyon' },
+           { src: 'ghost_town.jpg', srct: 'ghost_town.jpg', title: 'A Ghost Town' },
+           { src: 'jtree_laugh.jpeg', srct: 'jtree_laugh.jpeg', title: 'Joshua Tree' },
          ]
      });
 
    jQuery("#gallery_2").nanogallery2( {
-       // ### gallery settings ### 
-       thumbnailHeight:  150,
-       thumbnailWidth:   150,
-       thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
-       
-       // ### gallery content ### 
-       items: [
-           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
-         ]
-     });
+    thumbnailHeight:  300,
+    thumbnailWidth:   'auto',
+    thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+    thumbnailLabel: { display: false },
+    itemsBaseURL:     './assets/images/story/',
+
+    thumbnailHoverEffect2:      'scale120',
+    // ### gallery content ### 
+    items: [
+        { src: 'amy_clomp.jpg', srct: 'amy_clomp.jpg'},
+        { src: 'ryan-dyno.jpeg', srct: 'ryan-dyno.jpeg'},
+        { src: 'amy-clomp-2.jpeg', srct: 'amy-clomp-2.jpeg'},
+
+        { src: 'ryan-clomp-cropped.jpeg', srct: 'ryan-clomp-cropped.jpeg'},
+      ]
+  });
 
    jQuery("#gallery_3").nanogallery2( {
        // ### gallery settings ### 
@@ -37,100 +40,129 @@ jQuery(document).ready(function () {
        thumbnailWidth:   324 * 3,
        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
        itemsBaseURL:     './assets/images/story/',
-       
+       thumbnailHoverEffect2:      'scale120',
+
        // ### gallery content ### 
        items: [
-            { src: 'mt_tam.jpeg', srct: 'mt_tam_thumb.jpeg', title: 'First known photo of the couple' },
+            { src: 'mt_tam.jpeg', srct: 'mt_tam.jpeg', title: 'First known photo of the couple' },
          ]
      });
 
    jQuery("#gallery_4").nanogallery2( {
        // ### gallery settings ### 
-       thumbnailHeight:  150,
-       thumbnailWidth:   150,
+       thumbnailHeight:  300,
+       thumbnailWidth:   'auto',
        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       itemsBaseURL:     './assets/images/story/',
+       thumbnailHoverEffect2:      'scale120',
        
        // ### gallery content ### 
        items: [
-           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
-         ]
+        { src: 'elcap1.jpeg', srct: 'elcap1.jpeg'},
+        { src: 'elcap2.jpeg', srct: 'elcap2.jpeg'},
+        { src: 'elcap3.jpeg', srct: 'elcap3.jpeg'},
+        { src: 'elcap4.jpeg', srct: 'elcap4.jpeg'},
+      ]
      });
 
    jQuery("#gallery_5").nanogallery2( {
        // ### gallery settings ### 
-       thumbnailHeight:  150,
-       thumbnailWidth:   150,
+       thumbnailHeight:  300,
+       thumbnailWidth:   'auto',
        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       thumbnailLabel: { display: false },
+       itemsBaseURL:     './assets/images/story/',
+       thumbnailHoverEffect2:      'scale120',
        
        // ### gallery content ### 
        items: [
-           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+           { src: 'desolation.jpg', srct: 'desolation.jpg', title: 'Desolation Wilderness' },
+           { src: 'subdome.jpg', srct: 'subdome.jpg', title: 'Subdome' },
+           { src: 'half_dome_1.jpg', srct: 'half_dome_1.jpg', title: 'Half Dome' },
+           { src: 'jtree_1.jpeg', srct: 'jtree_1.jpeg', title: 'Joshua Tree' },
+           { src: 'jtree2.jpg', srct: 'jtree2.jpg', title: 'Joshua Tree' },
+           { src: 'lava_tube.jpg', srct: 'lava_tube.jpg', title: 'Lava Tubes' },
+           { src: 'kelso_dunes.jpeg', srct: 'kelso_dunes.jpeg', title: 'Kelso Dunes' },
+           { src: 'vernal_falls.jpg', srct: 'vernal_falls.jpg', title: 'Vernal Falls' },
+           { src: 'j_tree_3.jpg', srct: 'j_tree_3.jpg', title: 'Joshua Tree' },
+           { src: 'budd_creek.jpg', srct: 'budd_creek.jpg', title: 'Budd Creek' },
+           { src: 'lost_coast_2.jpg', srct: 'lost_coast_2.jpg', title: 'Lost Coast Trail' },
+           { src: 'half_dome_returns.jpg', srct: 'half_dome_returns.jpg', title: 'Half Dome' },
+           { src: 'ski_1.jpeg', srct: 'ski_1.jpeg', title: 'Skiing' },
          ]
      });
 
    jQuery("#gallery_6").nanogallery2( {
        // ### gallery settings ### 
-       thumbnailHeight:  150,
-       thumbnailWidth:   150,
+       thumbnailHeight:  300,
+       thumbnailWidth:   'auto',
        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-       itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+       itemsBaseURL:     './assets/images/story/',
+       thumbnailLabel: { display: false },
+       thumbnailHoverEffect2:      'scale120',
        
        // ### gallery content ### 
        items: [
-           { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-           { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-           { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+           { src: 'lost_coast_beach.jpg', srct: 'lost_coast_beach.jpg', title: 'Lost Coast Trail' },
+           { src: 'anza_stacked.jpg', srct: 'anza_stacked.jpg', title: 'Anza Borrego' },
+           { src: 'burney_falls.jpg', srct: 'burney_falls.jpg', title: 'Burney Falls' },
+           { src: 'shasta.jpg', srct: 'shasta.jpg', title: 'Mt. Shasta' },
+           { src: 'crater_lake.jpg', srct: 'crater_lake.jpg', title: 'Crater Lake' },
+           { src: 'golden_gate_1.jpg', srct: 'golden_gate_1.jpg', title: 'Golden Gate' },
          ]
      });
  
     jQuery("#gallery_7").nanogallery2( {
         // ### gallery settings ### 
-        thumbnailHeight:  150,
-        thumbnailWidth:   150,
+        thumbnailHeight:  300,
+        thumbnailWidth:   'auto',
         thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-        itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
+        itemsBaseURL:     './assets/images/story/',
+        thumbnailHoverEffect2:      'scale120',
         
         // ### gallery content ### 
         items: [
-            { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-            { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-            { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+            { src: 'clouds_rest_hike_1.jpeg', srct: 'clouds_rest_hike_1.jpeg', title: '' },
+            { src: 'clouds_rest_hike_2.jpeg', srct: 'clouds_rest_hike_2.jpeg', title: '' },
+            { src: 'clouds_rest_hike_0.jpeg', srct: 'clouds_rest_hike_0.jpeg', title: '' },
           ]
       });
  
     jQuery("#gallery_8").nanogallery2( {
         // ### gallery settings ### 
-        thumbnailHeight:  150,
-        thumbnailWidth:   150,
+        thumbnailHeight:  300,
+        thumbnailWidth:   360,
         thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-        itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
-        
+        thumbnailLabel: { display: false },
+        itemsBaseURL:     './assets/images/story/',
+        thumbnailHoverEffect2:      'scale120',
         // ### gallery content ### 
         items: [
-            { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-            { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-            { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
-          ]
+            { src: 'proposal_1.jpeg', srct: 'proposal_1.jpeg', title: '' },
+            { src: 'proposal_4.jpeg', srct: 'proposal_4.jpeg', title: '' },
+            { src: 'proposal_5.jpeg', srct: 'proposal_5.jpeg', title: '' },
+            { src: 'proposal_6.jpg', srct: 'proposal_6.jpg', title: '' },
+          ],
+        galleryMosaic :   [
+            { "c": 1, "r": 1, "w": 1, "h": 2 },
+            { "c": 2, "r": 1, "w": 1, "h": 1 },
+            { "c": 2, "r": 2, "w": 1, "h": 1 },
+            { "c": 3, "r": 1, "w": 1, "h": 2 },
+        ]
+        
       });
 
     jQuery("#gallery_9").nanogallery2( {
         // ### gallery settings ### 
-        thumbnailHeight:  150,
-        thumbnailWidth:   150,
+        thumbnailHeight:  960 / 1.5,
+        thumbnailWidth:   1280 / 1.5,
         thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
-        itemsBaseURL:     'https://nanogallery2.nanostudio.org/samples/',
-        
+        itemsBaseURL:     './assets/images/story/',
+        thumbnailLabel: { display: false },
+        thumbnailHoverEffect2:      'scale120',
         // ### gallery content ### 
         items: [
-            { src: 'berlin1.jpg', srct: 'berlin1_t.jpg', title: 'Berlin 1' },
-            { src: 'berlin2.jpg', srct: 'berlin2_t.jpg', title: 'Berlin 2' },
-            { src: 'berlin3.jpg', srct: 'berlin3_t.jpg', title: 'Berlin 3' }
+            { src: 'desert_kish.jpeg', srct: 'desert_kish_thumb.jpeg', title: 'Mesquite Dunes in Death Valley' },
           ]
       });
 
